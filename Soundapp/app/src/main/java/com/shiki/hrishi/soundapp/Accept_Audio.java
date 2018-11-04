4f3d24ea2ac04963d003349b753ffa233ab156d8
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2018-11-04 16:39:59 +0530

    Byte

---
+++ Soundapp/app/src/main/java/com/shiki/hrishi/soundapp/Accept_Audio.java
@@ -0,0 +1,17 @@
+package com.shiki.hrishi.soundapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Accept_Audio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept__audio);
+    }
+    public void sendMessage(View floatingactionbutton){
+    setContentView(R.layout.support_simple_spinner_dropdown_item);
+    }
+}
